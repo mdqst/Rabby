7892480851effe0ff49cda6f8227daa751303139
DMY <147dmy@gmail.com>
2024-09-03 17:48:21 +0800

    fix

--- src/ui/views/Approval/components/SignTx.tsx
+++ src/ui/views/Approval/components/SignTx.tsx
@@ -2007,6 +2007,7 @@ const SignTx = ({ params, origin }: SignTxProps) => {
             isWatchAddr={
               currentAccountType === KEYRING_TYPE.WatchAddressKeyring
             }
+            noCustomRPC={noCustomRPC}
             gasMethod={gasMethod}
             gasAccountCost={gasAccountCost}
             gasAccountCanPay={gasAccountCanPay}
